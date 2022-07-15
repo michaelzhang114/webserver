6078bcdaf19b7530e8937c9568c705a774970ba3
Michael Zhang <michaelzhang6284@gmail.com>
2022-07-14 17:32:01 -0700

    only accept GET

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
+use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::io::prelude::*;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -13,31 +13,37 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-
     // read each buffer stream
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let contents = fs::read_to_string("index.html").unwrap();
-
-    // return response
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        contents.len(),
-        contents
-    );
-
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-
-
+    let get = b"GET / HTTP/1.1\r\n";
+    // println!("{:?}", String::from_utf8_lossy(&buffer));
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("index.html").unwrap();
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let response = format!(
+            "{}\r\nContent-Length: {}\r\n\r\n{}",
+            status_line,
+            contents.len(),
+            contents
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 
     // response template:
     // Status line: http-version, status-code, reason-phrase, CRLF (character return line feed)
     // headers, CRLF
     // message-body
     // ex. HTTP/1.1 200 OK\r\n\r\n
-
-
 }
